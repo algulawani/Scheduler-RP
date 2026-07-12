9e4fa166270bfb34bd0c730b2d291043a3f0f5e7
anay <anay.gulawani@gmail.com>
2026-07-12 23:13:19 +0530

    Initial iteration of cpu util reader

---
+++ eBPF/src/cpu_utilization_pct.py
@@ -0,0 +1,71 @@
+# IMPORTANT:
+# This file is temporary!!! 
+# This uses BCC implementation and is only useful in for initial testing and debugging of the observation layer.
+# If for any reason this file exists along side a properly working .bpf.c file, then delete this.
+#	- Anay
+
+#!/usr/bin/env python3
+
+from bcc import BPF
+from time import sleep
+
+C_code = r"""
+BPF_ARRAY(time_stamps, __u64, 2);
+BPF_ARRAY(util_time, __u64, 2);
+
+TRACEPOINT_PROBE(sched, sched_switch)
+{
+	pid_t prev_PID;
+	u32 CPU;
+	u32 TIME_STAMPS_KEY_0 = 0;
+	u32 TIME_STAMPS_KEY_1 = 1;
+	u32 UTIL_TIME_KEY_0 = 0; //IDLE
+	u32 UTIL_TIME_KEY_1 = 1; //TOTAL
+	u64 prog_exec_cntr = 0;
+	__u64 initial_time;
+	__u64 current_time;
+	__u64 time_delta;
+	__u64 idle_time_passed = 0;
+	__u64 total_time_passed = 0;	
+
+	CPU = bpf_get_smp_processor_id();
+	if (CPU != 0)
+		return 0;
+
+	if (prog_exec_cntr == 0)
+	{
+		initial_time = bpf_ktime_get_ns();
+		time_stamps.update(&TIME_STAMPS_KEY_0, &initial_time);
+	}
+
+	current_time = bpf_ktime_get_ns();
+	time_delta = current_time - initial_time;
+	time_stamps.update(&TIME_STAMP_KEY_1, &current_time);
+	prev_PID = args->prev_pid;
+	total_time_passed += time_delta;
+
+	if (prev_PID == 0)
+		idle_time_passed += time_delta;
+
+	time_stamps.update(&TIME_STAMP_KEY_0, &current_time);
+	util_time.update(&UTIL_TIME_KEY_0, &idle_time_passed);
+	util_time.update(&UTIL_TIME_KEY_1, &total_time_passed);
+	prog_exec_cntr = 0;
+
+	return 0;
+}
+"""
+
+bpf = BPF(text=C_code)
+
+while True:
+	sleep(2)
+	idle_time_passed = bpf["util_time"][0].value
+	total_time_passed = bpf["util_time"][1].value
+	cpu_utilization_pct = (1 - idle_time_passed / total_time_passed) * 100
+	print(f"{cpu_utilization_pct} %")
+		
+	
+
+
+
