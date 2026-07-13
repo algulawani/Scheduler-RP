bd7c7fa45f06249f712af6cf42aa338efc2ff021
anay <anay.gulawani@gmail.com>
2026-07-13 10:33:55 +0530

    fixed (maybe) issue of overwriting total and idle time

--- eBPF/src/cpu_utilization_pct.py
+++ eBPF/src/cpu_utilization_pct.py
@@ -11,7 +11,6 @@ from time import sleep
 
 C_code = r"""
 BPF_ARRAY(prog_exec_cntr, u64, 1);
-BPF_ARRAY(time_acc, __u64, 2);
 BPF_ARRAY(time_stamps, __u64, 2);
 BPF_ARRAY(util_time, __u64, 2);
 
@@ -20,24 +19,29 @@ TRACEPOINT_PROBE(sched, sched_switch)
 	pid_t prev_PID;
 	u32 CPU;
 	u32 PROG_EXEC_CNTR_KEY_0 = 0;
-	u32 TIME_STAMPS_KEY_0 = 0;
-	u32 TIME_STAMPS_KEY_1 = 1;
-	u32 TIME_ACC_KEY_0 = 0; //IDLE
-	u32 TIME_ACC_KEY_1 = 1; //TOTAL
+	u32 TIME_STAMPS_KEY_0 = 0; //INITIAL
+	u32 TIME_STAMPS_KEY_1 = 1; //CURRENT
 	u32 UTIL_TIME_KEY_0 = 0; //IDLE
 	u32 UTIL_TIME_KEY_1 = 1; //TOTAL
-	u64 prog_exec_cntr = 0;
+	u64 *prog_exec_cntr_ptr;
 	__u64 initial_time;
 	__u64 current_time;
 	__u64 time_delta;
-	__u64 idle_time_passed = 0;
-	__u64 total_time_passed = 0;	
+	__u64 idle_time_passed;
+	__u64 total_time_passed;
+	__u64 *util_time_ptr;	
 
 	CPU = bpf_get_smp_processor_id();
 	if (CPU != 0)
 		return 0;
 
-	if (prog_exec_cntr == 0)
+	prog_exec_cntr_ptr = prog_exec_cntr.lookup(&PROG_EXEC_CNTR_KEY_0);
+	if (prog_exec_cntr_ptr == 0)
+		return 0;
+
+	prog_exec_cntr_val = *prog_exec_cntr_ptr;
+
+	if (prog_exec_cntr_val == 0)
 	{
 		initial_time = bpf_ktime_get_ns();
 		time_stamps.update(&TIME_STAMPS_KEY_0, &initial_time);
@@ -47,15 +51,30 @@ TRACEPOINT_PROBE(sched, sched_switch)
 	time_delta = current_time - initial_time;
 	time_stamps.update(&TIME_STAMPS_KEY_1, &current_time);
 	prev_PID = args->prev_pid;
+
+	util_time_ptr = util_time.lookup(&UTIL_TIME_KEY_1);
+	if (util_time_ptr == 0)
+		return 0;
+
+	total_time_passed = *util_time_ptr;
 	total_time_passed += time_delta;
+	util_time.update(&UTIL_TIME_KEY_1, &total_time_passed);
 
 	if (prev_PID == 0)
+	{
+		util_time_ptr = util_time.lookup(&UTIL_TIME_KEY_0);
+		if (util_time_ptr == 0)
+			return 0;
+		
+		idle_time_passed = *util_time_ptr;
 		idle_time_passed += time_delta;
+		util_time.update(&UTIL_TIME_KEY_0, &idle_time_passed);
+	}
 
 	time_stamps.update(&TIME_STAMPS_KEY_0, &current_time);
-	util_time.update(&UTIL_TIME_KEY_0, &idle_time_passed);
-	util_time.update(&UTIL_TIME_KEY_1, &total_time_passed);
-	prog_exec_cntr = 0;
+	
+	prog_exec_cntr_val++;
+	prog_exec_cntr.update(&PROG_EXEC_CNTR_KEY_0, &prog_exec_cntr_val);
 
 	return 0;
 }
